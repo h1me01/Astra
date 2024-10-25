92243c3becd057ceba8f02546da944b44f92cbad
h1me01 <Semih.Oezalp@hak-feldkirch.at>
2024-10-25 16:23:32 +0200

    update

--- src/uci.h
+++ src/uci.h
@@ -31,57 +31,6 @@ namespace UCI {
         Move getMove(const std::string &str_move) const;
     };
 
-    inline Move Uci::getMove(const std::string &str_move) const {
-        const Square from = squareFromString(str_move.substr(0, 2));
-        const Square to = squareFromString(str_move.substr(2, 2));
-        const Piece p_from = board.pieceAt(from);
-        const Piece p_to = board.pieceAt(to);
-
-        MoveFlags mf = QUIET;
-
-        const bool is_capture = p_to != NO_PIECE;
-
-        if (typeOf(p_from) == PAWN) {
-            const bool is_double_push = (rankOf(from) == 1 && rankOf(to) == 3) || (rankOf(from) == 6 && rankOf(to) == 4);
-            const bool is_promotion = rankOf(to) == 0 || rankOf(to) == 7;
-            const bool is_ep = board.history[board.getPly()].ep_sq == to;
-
-            if (is_double_push) {
-                mf = DOUBLE_PUSH;
-            } else if (is_ep) {
-                mf = EN_PASSANT;
-            } else if (is_promotion) {
-                char promotion = tolower(str_move[4]);
-
-                mf = is_capture ? PC_QUEEN : PR_QUEEN;
-                if (promotion == 'r') {
-                    mf = is_capture ? PC_ROOK : PR_ROOK;
-                } else if (promotion == 'b') {
-                    mf = is_capture ? PC_BISHOP : PR_BISHOP;
-                } else if (promotion == 'n') {
-                    mf = is_capture ? PC_KNIGHT : PR_KNIGHT;
-                }
-            } else if (is_capture) {
-                mf = CAPTURE;
-            }
-        } else if (typeOf(p_from) == KING) {
-            bool is_short = (from == e1 && to == g1) || (from == e8 && to == g8);
-            bool is_long = (from == e1 && to == c1) || (from == e8 && to == c8);
-
-            if (is_short) {
-                mf = OO;
-            } else if (is_long) {
-                mf = OOO;
-            } else if (is_capture) {
-                mf = CAPTURE;
-            }
-        } else if (is_capture) {
-            mf = CAPTURE;
-        }
-
-        return Move(from, to, mf);
-    }
-
 } // namespace UCI
 
 #endif //UCI_H
