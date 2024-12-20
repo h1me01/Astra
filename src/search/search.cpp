b6fe3f7ce4c0dc6d0c79eb5bdce46511644f5c6e
h1me01 <Semih.Oezalp@hak-feldkirch.at>
2024-12-20 21:22:35 +0100

    update

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -303,7 +303,7 @@ namespace Astra
                 return (eval + beta) / 2;
 
             // razoring
-            if (depth < 5 && eval + rzr_depth_mult * depth < alpha && alpha < VALUE_TB_WIN_IN_MAX_PLY)
+            if (depth < 5 && eval + rzr_depth_mult * depth < alpha)
             {
                 Score score = qSearch(0, alpha, beta, ss);
                 if (score <= alpha)
