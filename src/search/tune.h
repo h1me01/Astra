389f5122309cc9b0eaeeb54c6ae4e0f7282b6cf8
h1me01 <semih.oezalp@hak-feldkirch.at>
2025-04-24 11:30:14 +0200

    tune

--- src/search/tune.h
+++ src/search/tune.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-// #define TUNE
+ #define TUNE
 
 namespace Astra
 {
@@ -28,58 +28,58 @@ namespace Astra
 #define PARAM(name, value, min, max) constexpr int name = value
 #endif
 
-    PARAM(stability_base, 128, 100, 200);
-    PARAM(stability_mult, 71, 0, 250);
+    PARAM(stability_base, 130, 100, 200);
+    PARAM(stability_mult, 68, 0, 250);
 
     PARAM(results_base, 64, 0, 160);
-    PARAM(results_mult1, 9, 0, 27);
-    PARAM(results_mult2, 26, 0, 45);
-    PARAM(results_min, 68, 0, 150);
-    PARAM(results_max, 139, 50, 220);
+    PARAM(results_mult1, 10, 0, 27);
+    PARAM(results_mult2, 27, 0, 45);
+    PARAM(results_min, 69, 0, 150);
+    PARAM(results_max, 140, 50, 220);
 
-    PARAM(node_mult, 188, 120, 260);
-    PARAM(node_base, 48, 10, 90);
+    PARAM(node_mult, 187, 120, 260);
+    PARAM(node_base, 49, 10, 90);
 
-    PARAM(lmr_base, 104, 40, 200);
-    PARAM(lmr_div, 295, 150, 500);
+    PARAM(lmr_base, 106, 40, 200);
+    PARAM(lmr_div, 298, 150, 500);
 
     PARAM(static_h_mult, -58, -500, -1);
-    PARAM(static_h_min, 41, 1, 1000);
-    PARAM(static_h_max, 212, 1, 1000);
+    PARAM(static_h_min, 47, 1, 1000);
+    PARAM(static_h_max, 214, 1, 1000);
 
-    PARAM(rfp_depth_mult, 99, 40, 200);
+    PARAM(rfp_depth_mult, 100, 40, 200);
     PARAM(rfp_improving_mult, 83, 40, 200);
 
-    PARAM(rzr_depth_mult, 243, 150, 350);
+    PARAM(rzr_depth_mult, 245, 150, 350);
 
-    PARAM(nmp_depth_mult, 29, 1, 58);
-    PARAM(nmp_base, 143, 1, 400);
-    PARAM(nmp_eval_div, 208, 50, 350);
+    PARAM(nmp_depth_mult, 28, 1, 58);
+    PARAM(nmp_base, 139, 1, 400);
+    PARAM(nmp_eval_div, 209, 50, 350);
 
-    PARAM(hp_div, 7889, 1, 16384);
+    PARAM(hp_div, 8099, 1, 16384);
 
-    PARAM(fp_base, 138, 1, 300);
-    PARAM(fp_mult, 90, 5, 200);
+    PARAM(fp_base, 135, 1, 300);
+    PARAM(fp_mult, 89, 5, 200);
 
-    PARAM(see_cap_margin, 96, 5, 200);
-    PARAM(see_quiet_margin, 79, -50, 100);
+    PARAM(see_cap_margin, 93, 5, 200);
+    PARAM(see_quiet_margin, 80, -50, 100);
 
-    PARAM(ext_margin, 92, 10, 250);
-    PARAM(zws_margin, 55, 10, 160);
+    PARAM(ext_margin, 90, 10, 250);
+    PARAM(zws_margin, 51, 10, 160);
 
-    PARAM(hp_depth_mult, 6348, 2500, 12500);
-    PARAM(hp_qdiv, 6336, 1, 16384);
-    PARAM(hp_cdiv, 3805, 1, 16384);
-    PARAM(hbonus_margin, 55, 10, 200);
+    PARAM(hp_depth_mult, 6377, 2500, 12500);
+    PARAM(hp_qdiv, 6380, 1, 16384);
+    PARAM(hp_cdiv, 3935, 1, 16384);
+    PARAM(hbonus_margin, 54, 10, 200);
 
-    PARAM(qfp_margin, 104, 40, 280);
+    PARAM(qfp_margin, 105, 40, 280);
 
-    PARAM(history_bonus_mult, 405, 1, 1536);
-    PARAM(history_bonus_minus, -22, -500, 500);
-    PARAM(max_history_bonus, 2276, 1, 4096);
+    PARAM(history_bonus_mult, 428, 1, 1536);
+    PARAM(history_bonus_minus, -36, -500, 500);
+    PARAM(max_history_bonus, 2258, 1, 4096);
 
-    PARAM(history_malus_mult, 387, 1, 1536);
-    PARAM(history_malus_minus, 21, -500, 500);
-    PARAM(max_history_malus, 1842, 1, 4096);
+    PARAM(history_malus_mult, 395, 1, 1536);
+    PARAM(history_malus_minus, 24, -500, 500);
+    PARAM(max_history_malus, 1866, 1, 4096);
 
 } // namespace Astra
