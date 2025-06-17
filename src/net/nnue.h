4b5f3e07237bd3304ed3f466e48b9c9481f37f08
h1me01 <semih.oezalp@hak-feldkirch.at>
2025-06-17 10:45:26 +0200

    update

--- src/net/nnue.h
+++ src/net/nnue.h
@@ -34,17 +34,17 @@ constexpr int FT_QUANT = 255;
 constexpr int L1_QUANT = 64;
 
 // clang-format off
-    constexpr int KING_BUCKET[NUM_SQUARES]
-    {
-        0, 1, 2, 3, 3, 2, 1, 0,
-        4, 4, 5, 5, 5, 5, 4, 4,
-        6, 6, 6, 6, 6, 6, 6, 6,
-        7, 7, 7, 7, 7, 7, 7, 7,
-        8, 8, 8, 8, 8, 8, 8, 8,
-        8, 8, 8, 8, 8, 8, 8, 8,
-        9, 9, 9, 9, 9, 9, 9, 9,
-        9, 9, 9, 9, 9, 9, 9, 9,
-    };
+constexpr int KING_BUCKET[NUM_SQUARES]
+{
+    0, 1, 2, 3, 3, 2, 1, 0,
+    4, 5, 6, 7, 7, 6, 5, 4,
+    8, 8, 9, 9, 9, 9, 8, 8,
+    10,10,10,10,10,10,10,10,
+    10,10,10,10,10,10,10,10,
+    11,11,11,11,11,11,11,11,
+    11,11,11,11,11,11,11,11,
+    11,11,11,11,11,11,11,11,
+};
 // clang-format on
 
 inline bool needsRefresh(Piece pc, Square from, Square to) {
