9dfb5141062c50704706545050fed35de2cb10f9
Semih Özalp <124802233+h1me01@users.noreply.github.com>
2025-08-30 14:01:19 +0200

    use stricter tt depth requirement when tt_score <= beta

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -228,10 +228,10 @@ Score Search::negamax(int depth, Score alpha, Score beta, Stack *s, bool cut_nod
 
     if(!pv_node                                    //
        && !s->skipped                              //
-       && tt_depth >= depth                        //
        && board.get_fmr() < 90                     //
        && valid_score(tt_score)                    //
        && cut_node == (tt_score >= beta)           //
+       && tt_depth > depth - (tt_score <= beta)    //
        && valid_tt_score(tt_score, beta, tt_bound) //
     ) {
         return tt_score;
