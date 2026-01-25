ef8ef95a3d5de3cb654ee4b9e97acd4c07ca0658
h1me01 <semihozalp09@gmail.com>
2026-01-25 14:38:28 +0100

    update

--- src/nnue/constants.h
+++ src/nnue/constants.h
@@ -2,11 +2,11 @@
 
 namespace nnue {
 
-constexpr int INPUT_BUCKETS = 13;
+constexpr int INPUT_BUCKETS = 10;
 constexpr int FEATURE_SIZE = 768;
 
 constexpr int INPUT_SIZE = INPUT_BUCKETS * FEATURE_SIZE;
-constexpr int FT_SIZE = 1536;
+constexpr int FT_SIZE = 1024;
 constexpr int L1_SIZE = 16;
 constexpr int L2_SIZE = 32;
 constexpr int OUTPUT_BUCKETS = 8;
@@ -21,14 +21,14 @@ constexpr int ALIGNMENT = 64;
 constexpr int INT8_PER_INT32 = sizeof(int32_t) / sizeof(int8_t);
 
 constexpr int INPUT_BUCKET[64] = {
-    0,  1,  2,  3,  3,  2,  1,  0,  //
-    4,  5,  6,  7,  7,  6,  5,  4,  //
-    8,  8,  9,  9,  9,  9,  8,  8,  //
-    10, 10, 10, 10, 10, 10, 10, 10, //
-    11, 11, 11, 11, 11, 11, 11, 11, //
-    11, 11, 11, 11, 11, 11, 11, 11, //
-    12, 12, 12, 12, 12, 12, 12, 12, //
-    12, 12, 12, 12, 12, 12, 12, 12, //
+    0, 1, 2, 3, 3, 2, 1, 0, //
+    4, 4, 5, 5, 5, 5, 4, 4, //
+    6, 6, 6, 6, 6, 6, 6, 6, //
+    7, 7, 7, 7, 7, 7, 7, 7, //
+    8, 8, 8, 8, 8, 8, 8, 8, //
+    8, 8, 8, 8, 8, 8, 8, 8, //
+    9, 9, 9, 9, 9, 9, 9, 9, //
+    9, 9, 9, 9, 9, 9, 9, 9, //
 };
 
 constexpr float DEQUANT_MULT = (1 << FT_SHIFT) / static_cast<float>(FT_QUANT * FT_QUANT * L1_QUANT);
