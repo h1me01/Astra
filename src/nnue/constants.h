5bbbb5f81504dbb432402f4fc13100a0d7438c9b
h1me01 <semihozalp09@gmail.com>
2026-02-05 23:20:30 +0100

    new architecture, trained on about 350m newly generated positions (v6)
    Elo   | 33.05 +- 6.23 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 2594 W: 1352 L: 1106 D: 136
    Penta | [11, 32, 1064, 80, 110]
    
    bench: 2374412

--- src/nnue/constants.h
+++ src/nnue/constants.h
@@ -2,11 +2,29 @@
 
 namespace nnue {
 
-constexpr int INPUT_BUCKETS = 1;
 constexpr int FEATURE_SIZE = 768;
 
+constexpr int INPUT_BUCKET[64] = {
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+    0, 0, 0, 0, 0, 0, 0, 0, //
+};
+
+constexpr int INPUT_BUCKETS = []() constexpr {
+    int max = 0;
+    for (int i = 0; i < 64; ++i)
+        if (INPUT_BUCKET[i] > max)
+            max = INPUT_BUCKET[i];
+    return max + 1;
+}();
+
 constexpr int INPUT_SIZE = INPUT_BUCKETS * FEATURE_SIZE;
-constexpr int FT_SIZE = 1024;
+constexpr int FT_SIZE = 512;
 constexpr int L1_SIZE = 16;
 constexpr int L2_SIZE = 32;
 constexpr int OUTPUT_BUCKETS = 8;
@@ -19,17 +37,6 @@ constexpr int EVAL_SCALE = 400;
 
 constexpr int ALIGNMENT = 64;
 constexpr int INT8_PER_INT32 = sizeof(int32_t) / sizeof(int8_t);
-
-constexpr int INPUT_BUCKET[64] = {
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-    0, 0, 0, 0, 0, 0, 0, 0, //
-};
 
 constexpr float DEQUANT_MULT = (1 << FT_SHIFT) / static_cast<float>(FT_QUANT * FT_QUANT * L1_QUANT);
 
